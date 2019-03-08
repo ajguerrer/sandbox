4d7ac4276b6f236f756fe666e6b34c3a7dc62865
ajguerrer <ajguerrer@gmail.com>
2019-03-08 14:38:23 -0700

    setup worspace

---
+++ cpp/algorithms/binary-search/binary-search.h
@@ -0,0 +1,9 @@
+#include <cstddef>
+
+namespace alg {
+template <typename T, std::size_t N>
+int binary_search(T (&list)[N], T &item) {
+  int low = 0;
+  int high = len(list) - 1;
+}
+}  // namespace alg
