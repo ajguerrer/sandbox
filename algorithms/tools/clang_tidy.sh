daa891d7b6929dec8644582e3391bd859021f6df
Andrew Guerrero <ajguerrer@gmail.com>
2019-03-16 14:44:14 -0600

    remove bazel build system

--- algorithms/tools/clang_tidy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-CC=clang
-$(dirname "${BASH_SOURCE[0]}")/generate_compilation_database.sh
-
-/usr/local/share/clang/run-clang-tidy.py -p="$(bazel info execution_root)" -header-filter=.*
